a03839729484bf5069165af84c7308d6a8054892
Uchenna Ofor <ucheofor2015@gmail.com>
2024-11-28 17:42:36 +0100

    chore

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -11,50 +11,51 @@ const Support = () => {
         <h2 className=" w-full text-left text-gray-400 text-base my-5">
           Help categories
         </h2>
-        <div className="flex items-center mb-4 lg:mb-8">
-          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
-            <CiSettings className="w-6 h-6" />
+        <div className="flex flex-col gap-y-3">
+          
+          <div className="flex items-center mb-4 lg:mb-8">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
+              <CiSettings className="w-6 h-6" />
+            </div>
+            <div className=" w-[90%] flex flex-col gap-y-2">
+              <span>Getting Started</span>
+              <span className="text-xs text-gray-400">
+                Learn how to set up and begin using the agent platform.
+              </span>
+            </div>
           </div>
-          <div className="flex flex-col gap-y-2">
-            <span>Getting Started</span>
-            <span className="text-xs text-gray-400">
-              Learn how to set up and begin using the agent platform.
-            </span>
+          <div className="flex items-center mb-4 lg:mb-8">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
+              <LuCalendarCheck2 className="w-6 h-6" />
+            </div>
+            <div className=" w-[90%] flex flex-col gap-y-2">
+              <span>Referrals</span>
+              <span className="text-xs text-gray-400">
+                How to refer drivers and manage referrals.
+              </span>
+            </div>
           </div>
-        </div>
-
-        <div className="flex items-center mb-4 lg:mb-8">
-          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
-            <LuCalendarCheck2 className="w-6 h-6" />
+          <div className="flex items-center mb-4 lg:mb-8">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
+              <TbCreditCardOff className="w-6 h-6" />
+            </div>
+            <div className=" w-[90%] flex flex-col gap-y-2">
+              <span>Payment Issues</span>
+              <span className="text-xs text-gray-400">
+                Understand how earnings work and how to apply for withdrawals.
+              </span>
+            </div>
           </div>
-          <div className="flex flex-col gap-y-2">
-            <span>Referrals</span>
-            <span className="text-xs text-gray-400">
-              How to refer drivers and manage referrals.
-            </span>
-          </div>
-        </div>
-
-        <div className="flex items-center mb-4 lg:mb-8">
-          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
-            <TbCreditCardOff className="w-6 h-6" />
-          </div>
-          <div className="flex flex-col gap-y-2">
-            <span>Payment Issues</span>
-            <span className="text-xs text-gray-400">
-              Understand how earnings work and how to apply for withdrawals.
-            </span>
-          </div>
-        </div>
-        <div className="flex items-center">
-          <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
-            <MdOutlineSupportAgent className="w-6 h-6" />
-          </div>
-          <div className="flex flex-col gap-y-2">
-            <span>Technical Support</span>
-            <span className="text-xs text-gray-400">
-              Get help with app performance or technical issues.
-            </span>
+          <div className="flex items-center">
+            <div className="flex items-center justify-center w-10 h-10 rounded-full bg-indigo-100 text-indigo-600 font-bold mr-3">
+              <MdOutlineSupportAgent className="w-6 h-6" />
+            </div>
+            <div className=" w-[90%] flex flex-col gap-y-2">
+              <span>Technical Support</span>
+              <span className="text-xs text-gray-400">
+                Get help with app performance or technical issues.
+              </span>
+            </div>
           </div>
         </div>
       </div>
