b14ea01d6d60fcf433d232cef570de742ad5ea93
Uchenna Ofor <ucheofor2015@gmail.com>
2024-12-02 15:05:00 +0100

    notifications fixed

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -3,7 +3,7 @@ import { FaSpinner } from "react-icons/fa";
 
 const Spinner = () => {
   return (
-    <div className="flex justify-center flex-col text-center my-auto h-[500px] w-full">
+    <div className="flex justify-center flex-col text-center my-auto h-[390px] w-full">
       <FaSpinner
         className="animate-spin h-full my-auto mx-auto text-gray-500"
         size={24}
